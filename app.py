5e517a57040ba770d0b5f904e56336d25f05a9e1
Eero Koskinen <Eero Koskinen>
2025-01-22 19:24:37 +0200

    cleanup

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import os
-import secrets
-from flask import redirect, Blueprint
-from flask_login import LoginManager
+from flask import redirect, url_for
 from ac_dash import mk_ac_plot
 from ac_dash.api.routes import register_api, auth_bp
 from ac_dash.server import server, User, login_manager, api
@@ -11,21 +8,21 @@ from ac_dash.views.logout import mk_logout_page
 
 
 ac_plot_route = "/ac_dash/"
-mk_ac_plot(server, ac_plot_route)
-
-register_api(api)
 
 
 mk_login_page(server, "/login/")
 mk_logout_page(server, "/logout/")
 mk_success(server, "/success/")
 
+mk_ac_plot(server, ac_plot_route)
+register_api(api)
+
 server.register_blueprint(auth_bp)
 
 
 @server.route("/")
 def root():
-    return redirect(ac_plot_route)
+    return redirect(url_for(ac_plot_route))
 
 
 @login_manager.user_loader
@@ -34,4 +31,4 @@ def user_loader(user_id):
 
 
 if __name__ == "__main__":
-    server.run(debug=True)
+    server.run(host="0.0.0.0", debug=True)
